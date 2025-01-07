811150cd757edd812ef2e5dff5226697cc84b95a
kquach229 <kquach229@gmail.com>
2025-01-07 16:58:41 -0500

    save it

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,7 +1,9 @@
+'use client';
 import avatar1 from '@/assets/avatar-1.png';
 import avatar2 from '@/assets/avatar-2.png';
 import avatar3 from '@/assets/avatar-3.png';
 import avatar4 from '@/assets/avatar-4.png';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 const testimonials = [
@@ -42,7 +44,11 @@ export const Testimonials = () => {
           Our revolutionary AI SEO tools have transformed our clients
         </p>
         <div className='overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]'>
-          <div className='flex gap-5'>
+          <motion.div
+            initial={{ translateX: '-50%' }}
+            animate={{ translateX: '0' }}
+            transition={{ repeat: Infinity, ease: 'linear', duration: 30 }}
+            className='flex pr-5 gap-5 '>
             {testimonials.map((testimonial) => (
               <div
                 key={testimonial.name}
@@ -65,7 +71,7 @@ export const Testimonials = () => {
                 </div>
               </div>
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
